cb80df90d378acb30c499401d7f7895515ad5494
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2021-10-09 09:47:37 -0500

    nuevo texto

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -43,7 +43,7 @@ export class PingController {
   ping(): object {
     // Reply with a greeting, the current time, the url, and request headers
     return {
-      greeting: 'Hello from LoopBack, Andres',
+      greeting: 'Hello from LoopBack, Andres, nuevo texto',
       date: new Date(),
       url: this.req.url,
       headers: Object.assign({}, this.req.headers),
